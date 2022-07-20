859e8cff28007248fd2c017a4d26d8b01181898e
Mostafa Mahmoud <mostafa mahmoud@creditgo.app>
2022-07-20 15:39:02 +0200

    add config files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+  
+  const configService = app.get(ConfigService)
+  
+
+  const port =  configService.get<string>('PORT')
+  await app.listen(port, () => console.log(`app listening at port ${port} at ${new Date().toISOString()}`));
+  
 }
-bootstrap();
+bootstrap().catch(err => console.error(err));
+
